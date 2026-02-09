7caa78e18218c95334de0757d742db7173599420
tom <iza012209@gmail.com>
2026-02-09 21:32:00 +0700

    feat: Thêm các hàm sinh bảng tra tấn công

---
+++ ChessEngine/include/MoveGenerator.h
@@ -0,0 +1,19 @@
+#include "ChessDefinitions.h"
+
+
+namespace ChessEngine {
+
+	struct AttackTable {
+		u64 pawnAttack[2][64];
+		u64 kingAttack[64];
+		u64 knightAttack[64];
+		u64 rookAttack[64][4096];
+		u64 bishopAttack[64][512];
+
+		AttackTable();
+	};
+
+    void knightAttackTable(u64 (&KnightAttack)[64]);
+    void pawnAttackTable(u64 (&PawnAttack)[2][64]);
+    void kingAttackTable(u64 (&KingAttack)[64]);
+}
